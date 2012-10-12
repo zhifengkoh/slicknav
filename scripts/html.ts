0d299eaffedd9214d59e05aae23de42444e7f7fe
Zhifeng Koh <zhifeng.koh@moovweb.com>
2012-10-11 21:25:44 -0700

    Adding slicknav SCSS.

--- scripts/html.ts
+++ scripts/html.ts
@@ -3,14 +3,10 @@
 $("/html") {
   rewrite_links()
   absolutize_srcs()
-
-  
-
   add_assets()
 
   @import sections/header.ts
   @import sections/footer.ts
-
   @import mappings.ts
 }
 
--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,5 +1,34 @@
-# $("./body") {
-#   insert_bottom("footer", class: "_footer") {
-#     Move stuff here
-#   }
-# }
+$("body") {
+  insert_bottom("footer", class: "mw_footer") {
+    # Insert the input
+    insert("input", type: "checkbox", id: "menu", class: "menuControl", checked: "checked")
+    
+    # Insert the menu button
+    insert("label", for: "menu") {
+      insert("div", class: "circle-container") {
+        insert("button", class: "circle") {
+          text("✖")
+        }
+      }
+    }
+    
+    # Menu Items
+    insert("ul", class: "items") {
+      insert("li") {
+        text("★")
+      }
+      insert("li") {
+        text("★")
+      }
+      insert("li") {
+        text("★")
+      }
+      insert("li") {
+        text("★")
+      }
+      insert("li") {
+        text("★")
+      }
+    }
+  }
+}
