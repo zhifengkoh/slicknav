87fc187b127d0bff03e98611a6c9a355a372d713
Zhifeng Koh <zhifeng.koh@moovweb.com>
2012-10-12 00:42:36 -0700

    Adding footer.

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,5 +1,13 @@
 $("body") {
   insert_bottom("footer", class: "mw_footer") {
-    
+    insert("div", class: "mw_shoutout") {
+      insert("span") {
+        text("Powered by")
+      }
+      insert("img", src: asset("images/inset_grey_80x11.png"))
+      insert("div", class: "italicized") {
+        text("by Laurent Meynier & Zhifeng Koh")
+      }
+    }
   }
 }
