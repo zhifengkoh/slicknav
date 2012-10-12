a85fefd0f67362d55781555c4897a1aaffa44b88
Zhifeng Koh <zhifeng.koh@moovweb.com>
2012-10-11 23:04:23 -0700

    Moving slicknav to header (it's technically navigation)

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,15 +1,5 @@
 $("body") {
   insert_bottom("footer", class: "mw_footer") {
     
-    ur_slicknav() {                           # 1. Insert the slicknav
-      insert("ul", class: "items") {          # 2. Customize your menu!
-        ur_slicknav_item("/", "profile.png")
-        ur_slicknav_item("/", "add_user.png")
-        ur_slicknav_item("/", "users.png")
-        ur_slicknav_item("/", "add_project.png")
-        ur_slicknav_item("/", "home.png")
-      }
-    }
-    
   }
 }
--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,13 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+$("body") {
+  insert_top("header", class: "mw_header") {
+    ur_slicknav() {                           # 1. Insert the slicknav
+      insert("ul", class: "items") {          # 2. Customize your menu!
+        ur_slicknav_item("/users/1", "profile.png")
+        ur_slicknav_item("/users/new", "add_user.png")
+        ur_slicknav_item("/users", "users.png")
+        ur_slicknav_item("/projects/new", "add_project.png")
+        ur_slicknav_item("/", "home.png")
+      }
+    }
+  }
+}
